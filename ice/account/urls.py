8c197a636790263d39cf88fc50dee2ae3a78d338
HyDroGen <sir_ice39@outlook.com>
2019-09-18 01:28:31 +0700

    fix

--- ice/account/urls.py
+++ ice/account/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
+router.register(r'login', LoginView)
