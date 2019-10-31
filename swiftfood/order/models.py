51643093d822d9d2d7a7620e552b7183b5ab53b5
H2O <59010829@kmitl.ac.th>
2019-10-31 13:58:46 +0700

    add order field

--- swiftfood/order/models.py
+++ swiftfood/order/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 
 class OrderMenu(models.Model):
+    food_name = models.ForeignKey(null=True)
+    food_amount = models.SmallIntegerField(default=0, null=True)
+    price = models.SmallIntegerField()
