44849e0b6f2107a6cb2d0b133a2448d6bdde0c6f
HyDroGen <sir_ice39@outlook.com>
2019-11-07 23:05:17 +0700

    add field

--- swiftfood/order/admin.py
+++ swiftfood/order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import OrderMenu
+
+
+@admin.register(OrderMenu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('food_name',)
--- swiftfood/order/models.py
+++ swiftfood/order/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+from restaurant.swiftfood import menu
+
 
 class OrderMenu(models.Model):
+    food_name = models.ForeignKey(menu.menu_name.id, on_delete=models.CASCADE)
+    # price = models
