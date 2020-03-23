42a1f4b6c68d65c876e85657316e7eeea868982c
HyDroGeN <59010829@kmitl.ac.th>
2020-03-23 23:22:44 +0700

    ss

--- swiftfood/swiftfood/settings.py
+++ swiftfood/swiftfood/settings.py
@@ -123,6 +123,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
