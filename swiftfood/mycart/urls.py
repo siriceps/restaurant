a016b522e4688646ccfd025c1cf92b53432b8640
HyDroGeN <59010829@kmitl.ac.th>
2020-04-21 14:49:26 +0700

    fix

--- swiftfood/mycart/urls.py
+++ swiftfood/mycart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import MyCartMenuView, OrderMenuView
+from .view_order import OrderMenuView
+from .views import MyCartMenuView
 
 router = DefaultRouter()
 router.register(r'order', OrderMenuView)
